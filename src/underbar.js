3bf6c582093df22b1ecfca9c93a4c41363c2569d
Ryan Powell <try_try_again@protonmail.com>
2019-11-24 21:02:35 -0800

    Contains tests all pass

--- src/underbar.js
+++ src/underbar.js
@@ -185,6 +185,7 @@
   _.contains = function(collection, target) {
     // TIP: Many iteration problems can be most easily expressed in
     // terms of reduce(). Here's a freebie to demonstrate!
+    if (!Array.isArray(collection)) collection = Object.values(collection);
     return _.reduce(collection, function(wasFound, item) {
       if (wasFound) {
         return true;
